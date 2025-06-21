c69e1bfd45ef78a6d9c36bae7d6cd62113da6a79
frmdstryr <frmdstryr@protonmail.com>
2025-06-21 09:05:50 -0400

    Update import test

--- test/test_imports.py
+++ test/test_imports.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 import OCCT
 from os.path import abspath, dirname, split, splitext
@@ -8,18 +9,28 @@ class Test_ModulesImport(unittest.TestCase):
         errors = []
         root = OCCT.__path__[0]
 
+        if sys.platform == 'win32':
+            ext = 'pyd'
+        elif sys.platform == 'darwin':
+            ext = 'dylib'
+        else:
+            ext = 'so'
+
         mods = []
-        for f in glob(f'{root}/*'):
-            mod, ext = splitext(split(f)[1])
-            if mod.startswith("_") and ext:
+        for f in glob(f'{root}/*.{ext}'):
+            mod, _ = splitext(split(f)[1])
+            if mod.startswith("_"):
                 continue
             mods.append(f'OCCT.{mod}')
+        self.assertGreater(len(mods), 0, "No modules found")
+        mods.sort()
 
         for mod in mods:
             try:
                 __import__(mod)
+                print(f"Import {mod} ok")
             except ImportError as e:
-                if "Vtk" in mod:
+                if "Vtk" in mod or "OCCT.step" in mod:
                     pass # Ignore
                 else:
                     errors.append(mod)
